b57e220907e317a20e668e336c617753ebf28be4
Jonny <email@email.com>
2022-02-16 00:01:46 +0000

    Adding functionality to gameLib

--- contracts/libraries/gameLib.sol
+++ contracts/libraries/gameLib.sol
@@ -12,13 +12,14 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 library gameLib {
     //-------------------------Events-------------------------------//
+    event InjuredRaptor(uint16 raptor);
+    event FightWinner(uint16 raptor);
+
     event QuickPlayRaceStarted(uint16[] raptors, uint prizePool);
-    event QuickPlayRaceWinner(uint16 raptor, uint prize);
+    event QuickPlayRaceWinner(uint16 raptor, uint prize, address raptorOwner);
 
     event CompetitiveRaceStarted(uint16[] raptors, uint prizePool);
     event CompetitiveRaceWinner(uint16 raptor, uint prize);
-
-    event InjuredRaptor(uint16 raptor, uint time);
 
     event DeathRaceStarted(uint16[] raptors, uint prizePool);
     event DeathRaceWinner(uint16 raptor, uint prize);
@@ -34,7 +35,7 @@ library gameLib {
 
     //-------------------------Storage-------------------------------//
     //-------------------------Vars-------------------------------//
-
+    uint private distance;
     address private minterContract;
     address private payable communityWallet;
     //-------------------------Vars-------------------------------//
@@ -49,8 +50,16 @@ library gameLib {
         return true;
     }
 
+    function SetDistance(uint _distance) internal returns(bool){
+        distance = _distance;
+    }
+
     //-------------------------Setters-------------------------------//
     //-------------------------Getters-------------------------------//
+
+    function _distance() internal view pure returns(uint){
+        return distance;
+    }
 
     function _minter() internal view pure returns (address) {
         return minterContract;
@@ -88,6 +97,10 @@ library gameLib {
         fee = (pool / 100) * 5;
     }
 
+    function calcPrize(uint pool) internal  view pure returns (uint prize){
+        prize = (pool / 100) * 95;
+    }
+
     function getRandom(uint outOf) internal view returns(uint){
         return (SimpleOracleLibrary.getRandomNumber() % outOf) + 1;
     }
@@ -98,140 +111,139 @@ library gameLib {
         return true;
     }
 
+    function checkBounds(uint8 input) internal view pure returns(bool){
+        (input < 8 && input >= 0) ? return true; : return false;
+    }
+
     function getFighters() internal view pure returns(uint8 raptor1, uint8 raptor2){
-        uint rand = getRandom(147);
-        if(rand == 120 || rand == 0){
+        uint8 rand = getRandom(147);
+        while((rand % 40) == 0 || rand < 8){
             rand = getRandom(167);
         }
-        raptor1 = (rand % 5) ;
+        raptor1 = (rand % 5);
         raptor2 = (rand % 8);
-        uint8 counter = 7;
-        if (raptor1 == raptor2){
-            raptor1 = rand % 3;
+
+        if(raptor1 == raptor2){
+            raptor1 += rand % 3;
         }
+        check = checkBounds(raptor1);
+        if(!check) {
+            raptor1 =0 + (rand % 3)
+        } 
+    }
+
+    //agressiveness & strength not currently factors on who wins the fight
+    function getFightWinner(uint8 raptor1, uint8 raptor2) internal view pure returns(uint8){
+        (getRandom(2) == 0) ? return raptor1; : return raptor2; 
+    }
+
+    function getFastest(uint[] time, uint8[] indexesToIgnore) internal view pure returns (uint8 winner, uint8 second, uint8 third){
+        uint16 lowest=20000;
+        uint8 winner, second, third;
+        for(uint8 i =0; i< 8; i++){
+            if(i != indexesToIgnore[0] || i != indexesToIgnore[1]){
+                if(time[i]<lowest){
+                    third = second;
+                    second = winner;
+                    winner = i;
+                    lowest = time[i];            
+                }
+
+            }
+        }
+        return winner, second, third;
+    }
+
+    function getWinner(Stats[] stats, uint8[] indexesToIgnore) internal view pure returns(uint8 winner, uint8 second, uint8 third){
+        //get randomness for each raptor
+        uint8[] memory randomness = new uint8[](8);
+        for(uint i =0; i< 8; i++){
+            randomness[i] = getRandom(5);
+        }
+
+        //calc times to finish the race first with added randomness
+        uint[] memory time = new uint[](8);
+        for(uint8 i =0; i< 8; i++){
+            time[i] = _distance() / (stats[i].speed + randomness[i]);
+        }
+        
+        //gets fastest & ignores fighter indexes
+        return winner, second, third = getFastest(time, indexesToIgnore);
     }
 
     //-------------------------Helpers--------------------------------//
 
     //------------------------Stat-Changes------------------------------//
                        // -------  +vary ----------//
-    function upgradeAggressiveness(uint16 raptor) internal returns (bool){
-        uint rand = (getRandom(592) %3) +1;
-        stats = getStats(raptor);
+    function upgradeAggressiveness(Stats stats) internal returns(Stats){
+        uint8 rand = (getRandom(592) %3) +1;
         stats.aggressiveness += rand;
-        bool success = updateStats(stats,raptor);
-        require(success);
-        return true;
+        return stats;
     }
 
-    function upgradeStrength(uint16 raptor) internal returns (bool){
-        uint rand = (getRandom(768) %3) +1;
-        stats = getStats(raptor);
+    function upgradeStrength(Stats stats) internal returns(Stats){
+        uint8 rand = (getRandom(768) %3) +1;
         stats.strength += rand;
-        bool success = updateStats(stats,raptor);
-        require(success);
-        return true;
+        return stats;
     }
 
-    function upgradeSpeed(uint16 raptor) internal returns (bool){
-        uint rand = (getRandom(523) %3) +1;
-        stats = getStats(raptor);
+    function upgradeSpeed(Stats stats) internal returns(Stats){
+        uint8 rand = (getRandom(523) %3) +1;
         stats.speed += rand;
-        bool success = updateStats(stats,raptor);
-        require(success);
-        return true;
+        return stats;
     }
                        // -------  +Vary ----------//
 
-
-
-// struct Stats{
-//     uint16 speed,
-//     uint16 strength,
-//     uint16 agressiveness,
-//     uint16 fightsWon,
-//     uint16 fightsLost,
-//     uint16 quickPlayRacesWon,
-//     uint16 quickPlayRacesLost,
-//     uint16 compRacesWon,
-//     uint16 compRacesLost,
-//     uint16 deathRacesWon,
-//     uint16 deathRacesSurvived,
-//     uint16 deathRaceFightsWon,
-//     uint16 totalRacesTop3Finish,
-//     uint256 cooldownTime,
-
                        // -------  +1 ----------//
-    function increaseQPWins(uint16 raptor) internal returns(bool){
-        stats = getStats(raptor);
+    function increaseQPWins(Stats stats) internal returns(Stats){
         stats.quickPlayRacesWon += 1;
-        bool success = updateStats(stats,raptor);
-        require(success);
-        return true;
+        stats.totalRacesTop3Finish +=1;
+        return stats;
     }
 
-    function increaseQPLosses(uint16 raptor) internal returns(bool){
-        stats = getStats(raptor);
+    function increaseQPLosses(Stats stats) internal returns(Stats){
         stats.quickPlayRacesLost += 1;
-        bool success = updateStats(stats,raptor);
-        require(success);
-        return true;
+        return stats;
     }
 
-    function increaseCompWins(uint16 raptor) internal returns(bool){
-        stats = getStats(raptor);
+    function increaseCompWins(Stats stats) internal returns(Stats){
         stats.compRacesWon += 1;
-        bool success = updateStats(stats,raptor);
-        require(success);
-        return true;
+        stats.totalRacesTop3Finish += 1;
+        return stats;
     }
 
-    function increaseCompLosses(uint16 raptor) internal returns(bool){
-        stats = getStats(raptor);
+    function increaseCompLosses(Stats stats) internal returns(Stats){
         stats.compRacesLost += 1;
-        bool success = updateStats(stats,raptor);
-        require(success);
-        return true;
+        return stats;
     }
 
-    function increaseDeathRaceWins(uint16 raptor) internal returns(bool){
-        stats = getStats(raptor);
+    function increaseDeathRaceWins(Stats stats) internal returns(Stats){
         stats.deathRacesWon += 1;
         stats.deathRacesSurvived += 1;
-        bool success = updateStats(stats,raptor);
-        require(success);
-        return true;
-    }
-
-    function increaseDeathRacesSurvived(uint16 raptor) internal returns (bool){
-        stats = getStats(raptor);
-        stats.dathRacesSurvived += 1;
-        bool success = updateStats(stats,raptor);
-        require(success);
-        return true;
-    }
-
-    function increaseTop3RaceFinishes(uint16 raptor) internal returns(bool){
-        stats = getStats(raptor);
         stats.totalRacesTop3Finish += 1;
-        bool success = updateStats(stats,raptor);
-        require(success);
-        return true;
+        return stats;
+    }
+
+    function increaseDeathRacesSurvived(Stats stats) internal returns(Stats){
+        stats.dathRacesSurvived += 1;
+        return stats;
+    }
+
+    function increaseTop3RaceFinishes(Stats stats) internal returns(Stats){
+        stats.totalRacesTop3Finish += 1;
+        return stats;
     }
                        // -------  +1 ----------//
 
          // -----  +12 Hours/ Unless Founding Raptor 6 Hours -----//
     
-    function addCooldownPeriod(uint16 raptor) internal returns(bool){
-        stats = getStats(raptor);
+    function addCooldownPeriod(Stats stats) internal returns(Stats){
         if(stats.foundingRaptor == true){
             stats.cooldownTime = block.Timestamp() + 6 hours;
         } else {
             stats.cooldownTime += block.Timestamp() + 12 hours;
         }
-        bool success = updateStats(stats,raptor);
-        require(success);
-        return true;
+        return stats;
     }
          // -----  +12 Hours/ Unless Founding Raptor 6 Hours -----//
     //------------------------Stat-Changes---------------------------------//
@@ -240,17 +252,57 @@ library gameLib {
 
     //QP Start
     function _quickPlayStart(uint16[] raptors, uint prizePool) internal returns (bool){
+        emit QuickPlayRaceStarted(raptors,prizePool);
 
-    }
+        //get stats for each raptor
+        Stats[] memory stats = new Stats[](8);
+        for (uint8 i =0; i<8;i++){
+            stats[i] = getStats(raptors[i]);
+        }
 
-    //QP End
-    function _quickPlayEnd(uint16 winner, uint payout, uint communityPayout) internal payable returns(bool){
+        //gets fighters, finds the winner & adds them to indexes to ignore for choosing winner
+        uint8 raptor1, raptor2 = getFighters();
+        uint8 fightWinner = getFightWinner(raptor1, raptor2);
+        uint8[] indexesToIgnore = [raptor1, raptor2];
 
-    }
 
-    //QP Fight
-    function _quickPlayFight(uint16[] raptorsFighting) internal isTwo(raptorsFighting) {
-        winner
+        //gets the winner & next two places
+        uint8 raceWinner, second, third = getWinner(stats,indexesToIgnore);
+
+        //modify states
+        stats[raceWinner] = increaseQuickPlayWins(stats[raceWinner]);
+        stats[second] = increaseTop3RaceFinishes(stats[second]);
+        stats[third] = increaseTop3RaceFinishes(stats[third]);
+        stats[raceWinner] = increaseSpeed(stats[raceWinner]);
+        stats[fightWinner] = increaseStrength(stats[fightWinner]);
+
+        if(fightWinner == raptor1){ 
+            stats[raptor2] = increaseAggressiveness(stats[raptor2]); 
+            stats[raptor2] = increaseCooldownTime(stats[raptor2]);
+            emit InjuredRaptor(raptor2);
+        }else{
+            stats[raptor1] = increaseAggressiveness(stats[raptor1]);
+            stats[raptor1] = increaseCooldownTime(stats[raptor1]);
+            emit InjuredRaptor(raptor1);
+        }
+
+        emit FightWinner(fightWinner);        
+
+        //modify losses & update 
+        for(uint8 i =0; i<8; i++){
+            if(i != raceWinner){
+                stats[i] = increaseQuickPlayLosses(stats[i]);
+            }
+            updateStats(stats[i], raptors[i]);
+        }
+
+        //calculates rewards & pays out
+        uint fee = calcFee(prizePool);
+        uint prize = calcPrize(prizePool);
+        _payOut(raceWinner, prize, fee);
+
+        emit QuickPlayRaceWinner(raceWinner, prize, getOwner(raceWinner));
+
     }
 
     //---------------------------QP--------------------------------------//
@@ -261,16 +313,6 @@ library gameLib {
 
     }
 
-    //Comp End
-    function _compEnd(uint16 winner, uint payout, uint communityPayout) internal payable returns(bool){
-
-    }
-
-    //Comp Fight
-    function _compFight(uint16[] raptorsFighting) internal isTwo(raptorsFighting){
-
-    }
-
     //---------------------------------Comp--------------------------------//
     //-------------------------------DR------------------------------------//
 
@@ -278,23 +320,6 @@ library gameLib {
     function _deathRaceStart(uint16[] raptors, uint prizePool) internal returns(bool){
 
     }
-
-    //DR End
-    function _deathRaceEnd(uint16 winner, uint payout, uint communityPayout) internal payable returns(bool){
-
-    }
-
-    //DR Fight
-    function _fightToTheDeath(uint16[] raptorsFighting) internal isTwo(raptorsFighting) {
-
-    }
-
-
-    // function safeTransferFrom(
-    //     address from,
-    //     address to,
-    //     uint256 tokenId
-    // ) external;
 
     //DR Kill/BURN RAPTOR
     function _kill(uint16 raptor) internal returns (bool){
